f8e0fd83b31517faeb74861111542e564da61d22
jilllllllllllllllllllllllllllllllllllll <tkmdrhtt@gmail.com>
2015-11-04 10:47:59 +0800

    Pages update
    +addBook.html
    +add Genre field
    =Segregated user/admin navbars

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,22 +1,37 @@
 $(document).ready(function() {
-	$('ul.library').click(function(event){
+	$('ul.search').click(function(event){
 		$(this).css('background', '#2a6cbe');
-		$('.library .menu').css('display', 'block');
+		$('.search .menu').css('display', 'block');
 		$('.students .menu').css('display', 'none');
+		$('.add .menu').css('display', 'none');
 		$('ul.students').css('background', 'none');
+		$('ul.add').css('background', 'none');
 		event.stopPropagation();
 	});
 	$('ul.students').click(function(event){
 		$(this).css('background', '#2a6cbe');
 		$('.students .menu').css('display', 'block');
-		$('.library .menu').css('display', 'none');
-		$('ul.library').css('background', 'none');
+		$('.search .menu').css('display', 'none');
+		$('.add .menu').css('display', 'none');
+		$('ul.search').css('background', 'none');
+		$('ul.add').css('background', 'none');
+		event.stopPropagation();
+	});
+	$('ul.add').click(function(event){
+		$(this).css('background', '#2a6cbe');
+		$('.add .menu').css('display', 'block');
+		$('.students .menu').css('display', 'none');
+		$('.search .menu').css('display', 'none');
+		$('ul.students').css('background', 'none');
+		$('ul.search').css('background', 'none');
 		event.stopPropagation();
 	});
 	$(document).click(function(){
 		$('ul.students').css('background', 'none');
-		$('ul.library').css('background', 'none');
+		$('ul.search').css('background', 'none');
+		$('ul.add').css('background', 'none');
 		$('.students .menu').css('display', 'none');
-		$('.library .menu').css('display', 'none');
+		$('.search .menu').css('display', 'none');
+		$('.add .menu').css('display', 'none');
 	});
 });
